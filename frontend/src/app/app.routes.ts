eb99da2c725c25e4cba01557e025763031e03a00
Lexleon Oliver <lexleonmg@gmail.com>
2026-09-07 22:30:02 -0300

    feat: refactor login and dashboard components with improved UI and functionality

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { Forbidden } from './core/pages/forbidden/forbidden';
 import { NotFound } from './core/pages/not-found/not-found';
-import { DashboardComponent } from './features/auth/login/dashboard/dashboard-component/dashboard-component';
 import { authGuard } from './core/guards/auth.guard';
+import { DashboardComponent } from './features/dashboard/dashboard-component/dashboard-component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: 'dashboard',
     component: DashboardComponent,
-    canActivate: [authGuard] // <-- Protege o acesso direto
+    canActivate: [authGuard]
   },
   { path: 'login', component: Login },
   { path: '403', component: Forbidden },
